1f3f7ba0a197e094cfed70d7155f97cf526f0a89
Sitaram0104 <meenasitu0104@gmail.com>
2021-01-10 22:36:33 +0530

    tutorial20

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
 import User from "../models/userModel.js";
 import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../util.js";
 
 const userRouter = express.Router();
 
@@ -13,5 +15,26 @@ const userRouter = express.Router();
 //     res.send({ createdUsers });
 //   })
 // );
+
+// to generate new resource/token
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
 
 export default userRouter;
